1e716807a37423d89db80708ecb6c6b0d7414161
helio <heliojmsemedo@gmail.com>
2021-06-07 20:44:07 +0100

    refactor: more logging

--- Ambedo.API/Controllers/ThootlesController.cs
+++ Ambedo.API/Controllers/ThootlesController.cs
@@ -2,6 +2,7 @@
 using Ambedo.Services.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,52 +10,65 @@ using Dtos = Ambedo.Contract.Dtos;
 
 namespace Ambedo.API.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ThootlesController : Controller
-    {
-        protected readonly IThootlesService _service;
-        protected readonly IMapper _mapper;
-        public ThootlesController(IThootlesService thootlesService, IMapper mapper)
-        {
-            _service = thootlesService;
-            _mapper = mapper;
-        }
+    //@TODO: refactor and enhance logging for this controller, possibly into a middleware, serilog is nice but doesnt do all the work
+	[ApiController]
+	[Route("[controller]")]
+	public class ThootlesController : Controller
+	{
+		protected readonly IThootlesService _service;
+		protected readonly IMapper _mapper;
+		public ThootlesController(IThootlesService thootlesService, IMapper mapper)
+		{
+			_service = thootlesService;
+			_mapper = mapper;
+		}
 
-        [HttpPost]
-        public async Task<ActionResult<Dtos.Thootle>> Post([FromBody] Dtos.UnidentifiedThootle data)
-        {
-            var result = await _service.CreateThootle(_mapper.Map<Dtos.UnidentifiedThootle, Thootle>(data));
-            return CreatedAtAction(nameof(GetOne), new { id = result.Id }, _mapper.Map<Thootle, Dtos.Thootle>(result));
-        }
+		[HttpPost]
+		public async Task<ActionResult<Dtos.Thootle>> Post([FromBody] Dtos.UnidentifiedThootle data)
+		{
+			var result = await _service.CreateThootle(_mapper.Map<Dtos.UnidentifiedThootle, Thootle>(data));
+			return CreatedAtAction(nameof(GetOne), new { id = result.Id }, _mapper.Map<Thootle, Dtos.Thootle>(result));
+		}
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Dtos.Thootle>>> Get([FromQuery] string filter)
-        {
-            var result = await _service.GetThootles(filter);
-            var response = _mapper.Map<IEnumerable<Thootle>, IEnumerable<Dtos.Thootle>>(result);
-            return Ok(response);
-        }
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Dtos.Thootle>>> Get([FromQuery] string filter)
+		{
+			var result = await _service.GetThootles(filter);
+			var response = _mapper.Map<IEnumerable<Thootle>, IEnumerable<Dtos.Thootle>>(result);
+			return Ok(response);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Dtos.Thootle>> GetOne([FromRoute] string id)
-        {
-            var result = await _service.GetThootle(id);
-            return Ok(_mapper.Map<Thootle, Dtos.Thootle>(result));
-        }
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Dtos.Thootle>> GetOne([FromRoute] string id)
+		{
+			if (!ObjectId.TryParse(id, out _))
+			{
+				return BadRequest(new { error = $"Provided id '{id}' is not a valid id. Id must be a 24 digit hex string." });
+			}
+			var result = await _service.GetThootle(id);
+			return Ok(_mapper.Map<Thootle, Dtos.Thootle>(result));
+		}
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult<Dtos.ReplaceOneResult>> Put([FromRoute] string id, [FromBody] Dtos.UnidentifiedThootle data)
-        {
-            var result = await _service.UpdateThootle(id, _mapper.Map<Dtos.UnidentifiedThootle, Thootle>(data));
-            return Ok(_mapper.Map<ReplaceOneResult, Dtos.ReplaceOneResult>(result));
-        }
+		[HttpPut("{id}")]
+		public async Task<ActionResult<Dtos.ReplaceOneResult>> Put([FromRoute] string id, [FromBody] Dtos.UnidentifiedThootle data)
+		{
+			if (!ObjectId.TryParse(id, out _))
+			{
+				return BadRequest(new { error = $"Provided id '{id}' is not a valid id. Id must be a 24 digit hex string." });
+			}
+			var result = await _service.UpdateThootle(id, _mapper.Map<Dtos.UnidentifiedThootle, Thootle>(data));
+			return Ok(_mapper.Map<ReplaceOneResult, Dtos.ReplaceOneResult>(result));
+		}
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Dtos.DeleteResult>> Delete([FromRoute] string id)
-        {
-            var result = await _service.DeleteThootle(id);
-            return Ok(_mapper.Map<DeleteResult, Dtos.DeleteResult>(result));
-        }
-    }
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Dtos.DeleteResult>> Delete([FromRoute] string id)
+		{
+			if (!ObjectId.TryParse(id, out _))
+			{
+				return BadRequest(new { error = $"Provided id '{id}' is not a valid id. Id must be a 24 digit hex string." });
+			}
+			var result = await _service.DeleteThootle(id);
+			return Ok(_mapper.Map<DeleteResult, Dtos.DeleteResult>(result));
+		}
+	}
 }
--- Ambedo.Services/ThootleService.cs
+++ Ambedo.Services/ThootleService.cs
@@ -27,21 +27,21 @@ namespace Ambedo.Services
 		public async Task<Thootle> CreateThootle(Thootle data)
 		{
 			await _thootles.InsertOneAsync(data);
-			Log.Debug("CreateThootle {@data}", data);
+			Log.Debug("CRUD CreateThootle Thootle:{@data}", data);
 			return await Task.FromResult(data);
 		}
 
 		public async Task<IEnumerable<Thootle>> GetThootles(FilterDefinition<Thootle> filter)
 		{
 			var items = (await _thootles.FindAsync(filter ?? FilterDefinition<Thootle>.Empty)).ToList();
-			Log.Debug("CRUD GetThootles Filter:{Filter}, Count:{Count}, Thootles:{@Thootles}", filter, items.Count, items);
+			Log.Debug("CRUD GetThootles Filter:{Filter}; Count:{Count}; Thootles:{@Thootles}", filter, items.Count, items);
 			return await Task.FromResult(items);
 		}
 
 		public async Task<IEnumerable<Thootle>> GetThootles()
 		{
 			var items = (await _thootles.FindAsync(FilterDefinition<Thootle>.Empty)).ToList();
-			Log.Debug("CRUD GetThootles Count:{Count}, Thootles:{@Thootles}", items.Count, items);
+			Log.Debug("CRUD GetThootles Count:{Count}; Thootles:{@Thootles}", items.Count, items);
 			return await Task.FromResult(items);
 		}
 
@@ -55,21 +55,21 @@ namespace Ambedo.Services
 		public async Task<ReplaceOneResult> UpdateThootle(string id, Thootle data)
 		{
 			var result = await _thootles.ReplaceOneAsync(GetIdFilter(id), data);
-			Log.Debug("CRUD UpdateThootle Id:{Id}, Thootle:{@Thootle}, Result:{@Result}", id, data, result);
+			Log.Debug("CRUD UpdateThootle Id:{Id}; Thootle:{@Thootle}; Result:{@Result}", id, data, result);
 			return await Task.FromResult(result);
 		}
 
 		public async Task<ReplaceOneResult> UpdateThootle(Thootle data)
 		{
 			var result = await UpdateThootle(data.Id, data);
-			Log.Debug("CRUD UpdateThootle Thootle:{@Thootle}, Result:{@Result}", data, result);
+			Log.Debug("CRUD UpdateThootle Thootle:{@Thootle}; Result:{@Result}", data, result);
 			return await Task.FromResult(result); ;
 		}
 
 		public Task<DeleteResult> DeleteThootle(string id)
 		{
 			var result = _thootles.DeleteOneAsync(GetIdFilter(id));
-			Log.Debug("CRUD DeleteThootle Id:{Id}, Result:{@Result}", id, result);
+			Log.Debug("CRUD DeleteThootle Id:{Id}; Result:{@Result}", id, result);
 			return _thootles.DeleteOneAsync(GetIdFilter(id));
 		}
 
